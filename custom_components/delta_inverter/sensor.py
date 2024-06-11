a8be6fa3a2b986958d0702ddf0a26c0a9492392e
Matěj Hloušek <matej@bnet-internet.cz>
2024-06-11 22:42:24 +0200

    uprava

--- custom_components/delta_inverter/sensor.py
+++ custom_components/delta_inverter/sensor.py
@@ -115,7 +115,7 @@ class DeltaInverterSensor(Entity):
         return crc
 
 
-    def create_query(address, command, sub_command, data=b''):
+    def create_query(self, address, command, sub_command, data=b''):
         stx = 0x02
         enq = 0x05
         etx = 0x03
@@ -126,7 +126,7 @@ class DeltaInverterSensor(Entity):
             'B', sub_command) + data
 
         # Calculate CRC
-        crc = self.calc_crc(frame[1:])  # Exclude the first byte (STX) from CRC calculation
+        crc = calc_crc(frame[1:])  # Exclude the first byte (STX) from CRC calculation
         crc_low = crc & 0xFF
         crc_high = (crc >> 8) & 0xFF
 
@@ -153,7 +153,7 @@ class DeltaInverterSensor(Entity):
 
     def parse_response(self, response):
 
-        parsed_data = self.parse_data(data)
+        parsed_data = parse_data(data)
         state = parsed_data['AC Power']
         attributes = parsed_data
         return state, attributes
